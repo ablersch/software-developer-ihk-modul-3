14002439d3890d498771320ef161135aa1eb1463
ablersch <andreas@blersch.org>
2025-01-11 09:51:33 +0100

    Add excersice

---
+++ exercises/Uebung_01_Umrechnung/Loesung_1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+
+namespace Umrechnung;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnC2F_Click(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(tbxGrad.Text))
+        {
+            if (double.TryParse(tbxGrad.Text, out double input))
+            {
+                input = (input*9/5) + 32;
+                input = Math.Round(input, 2);
+
+                tbxResult.Text = $"{input} F";
+            }
+            else
+            {
+                MessageBox.Show("Bitte nur Zahlen eingeben", "Eingabefehler", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            MessageBox.Show("Bitte eine Zahl eingeben", "Eingabefehler", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void btnExit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void btnF2C_Click(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(tbxGrad.Text))
+        {
+            if (double.TryParse(tbxGrad.Text, out double input))
+            {
+                input = ((input - 32)*5)/9;
+                input = Math.Round(input, 2);
+
+                tbxResult.Text = $"{input} C";
+            }
+            else
+            {
+                MessageBox.Show("Bitte nur Zahlen eingeben", "Eingabefehler", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            MessageBox.Show("Bitte eine Zahl eingeben", "Eingabefehler", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ exercises/Uebung_02_Taschenrechner/README.md
@@ -0,0 +1,15 @@
+# Übung - Taschenrechner
+
+Erstellen Sie einen Taschenrechner der die Grundrechenarten beherrscht.
+
+Sie können die GUI frei nach Ihren Wünschen gestalten. Um das Programm einfacher zuhalten können Sie drei Textfelder anstatt einem Textfeld (wie im Beispiel) nutzen.
+
+## Hinweise
+
+* Benutzen Sie als Layout kein `Canvas`.
+* Fehlerhafte und ungültige Eingaben sollen abgefangen und eine Meldung ausgegeben werden.
+* Achten Sie außerdem darauf, dass ein Teilen durch 0 nicht möglich ist.
+
+## Beispiel
+
+![Beispiel UI](Bild1.png)
